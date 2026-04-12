cf8bebb319e80eb7a9528305096d2a0e4419c7e1
dinesh483 <yessdinesh3@gmail.com.>
2026-04-12 10:51:59 +0530

    progress for auth

--- databasemodel.py
+++ databasemodel.py
@@ -1,7 +1,16 @@
-from sqlalchemy import Column, Integer, Text, String, Boolean, Date
+from sqlalchemy import Column, Integer, Text, String, Boolean, Date,ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+class user(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    username = Column(String(50), unique=True, index=True)
+    hashed_password = Column(String(500))
+
+    tasks = relationship("Todo", back_populates="owner")
 
 class Todo(Base):
     __tablename__ = "list"
@@ -13,6 +22,8 @@ class Todo(Base):
     created_at = Column(Date)
     priority = Column(String(10))
     due_date = Column(Date)
+    owner_id = Column(Integer, ForeignKey("users.id"),nullable=True)
+    owner = relationship("user",back_populates="tasks")
 
 
 
--- models.py
+++ models.py
@@ -2,6 +2,17 @@ from pydantic import BaseModel
 from datetime import date
 from typing import Optional
 
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+
+    class Config:
+        from_attributes = True
+
 class tasks(BaseModel):
    task_id: Optional[int] = None
    task_name: str
@@ -10,3 +21,4 @@ class tasks(BaseModel):
    created_at: date
    priority: str
    due_date: date
+   owner_id: Optional[int] = None
